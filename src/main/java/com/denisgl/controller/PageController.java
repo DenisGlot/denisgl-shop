6b5e4a70b658e755675ec8fbf241bc4317b05e2a
dennis <denisglotov98@mail.ru>
2018-05-13 21:47:19 +0300

    unfinished static

--- src/main/java/com/denisgl/controller/PageController.java
+++ src/main/java/com/denisgl/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.denisgl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,13 @@ public class PageController {
     public ModelAndView hello() {
         ModelAndView modelAndView = new ModelAndView("page");
         modelAndView.addObject("greeting", "Welcome to spring mvc");
+        return modelAndView;
+    }
+
+    @RequestMapping("/test/{param}")
+    public ModelAndView test(@PathVariable("param") String param) {
+        ModelAndView modelAndView = new ModelAndView("page");
+        modelAndView.addObject("greeting", param);
         return modelAndView;
     }
 }
